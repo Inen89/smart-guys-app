47cddc7e7703c711b76d051a07d3ebceb7085d8f
Inen89 <grzeglos@gmail.com>
2025-12-11 22:09:50 +0100

    feat(api): add private API key to ApiService for Reqres requests

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -7,26 +7,43 @@ import { Observable } from 'rxjs';
 })
 export class ApiService {
   private baseUrl = 'https://reqres.in/api';
+  private apiKey = 'reqres_4a7c9c9f887445a8b507d0c84a61aae0';
 
   constructor(private http: HttpClient) {}
 
   // Pobieranie listy użytkowników
   getUsers(): Observable<any> {
-    return this.http.get(`${this.baseUrl}/users`);
+    return this.http.get(`${this.baseUrl}/users`, {
+      headers: {
+        'X-API-Key': this.apiKey,
+      },
+    });
   }
 
   // Dodawanie nowego użytkownika
   addUser(userData: any): Observable<any> {
-    return this.http.post<any>(`${this.baseUrl}/users`, userData);
+    return this.http.post<any>(`${this.baseUrl}/users`, userData, {
+      headers: {
+        'X-API-Key': this.apiKey,
+      },
+    });
   }
 
   // Edytowanie istniejącego użytkownika
   updateUser(userId: number, userData: any): Observable<any> {
-    return this.http.put(`${this.baseUrl}/users/${userId}`, userData);
+    return this.http.put(`${this.baseUrl}/users/${userId}`, userData, {
+      headers: {
+        'X-API-Key': this.apiKey,
+      },
+    });
   }
 
   // Usuwanie użytkownika
   deleteUser(userId: number): Observable<any> {
-    return this.http.delete(`${this.baseUrl}/users/${userId}`);
+    return this.http.delete(`${this.baseUrl}/users/${userId}`, {
+      headers: {
+        'X-API-Key': this.apiKey,
+      },
+    });
   }
 }
